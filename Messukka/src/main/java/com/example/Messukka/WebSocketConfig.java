dd7cb23d69023e047c555298987982b5e31299b5
jartsanull <langinenj1@gmail.com>
2024-08-14 11:40:40 +0300

    Update WebSocketConfig.java

--- Messukka/src/main/java/com/example/Messukka/WebSocketConfig.java
+++ Messukka/src/main/java/com/example/Messukka/WebSocketConfig.java
@@ -12,15 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-	
 		config.enableSimpleBroker("/topic");
 		config.setApplicationDestinationPrefixes("/app");
-	
 	}
 	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-	
 		registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
 	}
 	
